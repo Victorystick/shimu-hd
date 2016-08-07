29152b0813dc9554b336b0fb1413f6349f9c5b8a
Johan Fogelström <zapmew@gmail.com>
2016-08-07 22:36:21 +0200

    Added Spawn algorithm.

--- src/Game.js
+++ src/Game.js
@@ -16,6 +16,10 @@ export class Game {
     this.boundTick = () => this.tick();
   }
 
+  getSize() {
+    return this.canvas;
+  }
+
   start() {
     this.player = new Shimu(new Vec2(50, 50), this.controls);
     this.entities.push(this.player);
@@ -31,6 +35,20 @@ export class Game {
       requestAnimationFrame(this.boundTick);
     }
   }
+}
+
+function spawnEnemies(game, level) {
+  for (var i = 0; i < 30; i++) {
+    game.entities.push(new Enemy(bogoSpawn(game.getSize(), game.player), 5, 'yellow', 0.05 + 0.002*level))
+  }
+}
+
+function bogoSpawn(size, player) {
+  var position = new Vec2(0, 0);
+  do {
+    position.x = Math.random()*size.width;
+    position.y = Math.random()*size.height;
+  } while (Vec2.sub(position, player.position).length() < 100)
 }
 
 /**
