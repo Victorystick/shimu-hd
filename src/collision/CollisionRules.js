b28fa7524da13f2baa6952144f0fdbf3837ab20c
Johan Fogelström <zapmew@gmail.com>
2016-08-13 21:20:20 +0200

    Added game as first argument to collision functions.

--- src/collision/CollisionRules.js
+++ src/collision/CollisionRules.js
@@ -4,17 +4,17 @@ export class CollisionRules {
   }
 
   add(type1, type2, collision_fn) {
-    this.collisions.push((e1, e2) => {
+    this.collisions.push((game, e1, e2) => {
       if (e1 instanceof type1 && e2 instanceof type2) {
-        return collision_fn(e1, e2);
+        return collision_fn(game, e1, e2);
       }
       return true;
     });
   }
 
-  onCollide(entity1, entity2) {
+  onCollide(game, entity1, entity2) {
     for (var fn of this.collisions) {
-      if (fn(entity1, entity2)) continue;
+      if (fn(game, entity1, entity2)) continue;
       return;
     }
   }
