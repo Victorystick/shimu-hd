ba15b717f89beddca7aae6f73bc22742b5b19c89
Oskar Segersvärd <oseg@google.com>
2016-08-14 21:32:32 +0200

    Implement touch controls, and use them when available

--- src/main.js
+++ src/main.js
@@ -4,10 +4,16 @@ import {Logic} from './Logic.js';
 import * as controls from './controls.js';
 import {createAnimationTicker} from './tickers.js';
 
+function getControls() {
+  return 'ontouchend' in window ? controls.Touch : controls.Keyboard;
+}
+
 export default {
   start(canvas) {
+    const Controls = getControls();
+
     const logic = new Logic(Modern);
-    const ctrl = new controls.Keyboard(canvas);
+    const ctrl = new Controls(canvas);
     const ticker = createAnimationTicker();
     new Game(canvas.getContext('2d'), logic, ctrl, ticker).start();
   },
