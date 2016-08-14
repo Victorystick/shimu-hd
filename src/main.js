f12c6792d52610a8be21507ff4600303308789d4
Johan Fogelström <zapmew@gmail.com>
2016-08-14 16:39:43 +0200

    Forgot to import Modern in main.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import {Game} from './Game.js';
+import {Modern} from './collision/modern.js';
 import {Logic} from './Logic.js';
 import * as controls from './controls.js';
 import {createAnimationTicker} from './tickers.js';
